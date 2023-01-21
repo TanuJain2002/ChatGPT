2320a7b32e4dfeecc6b664a551284a0676030e5a
Tanu Jain <tanujain13092@gmail.com>
2023-01-21 02:49:29 -0800

    first commit

---
+++ lib/api_services.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+String apiKey = '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';
+//head over to openai.com -> signIn/signUp -> API -> (Top right)Personal -> View API keys -> Create new Secret API key
+
+class ApiServices {
+  static String baseUrl = "https://api.openai.com/v1/completions";
+
+  static Map<String, String> header = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer $apiKey'
+  };
+  static sendMessage(String? message) async {
+    var res = await http.post(Uri.parse(baseUrl),
+        headers: header,
+        body: jsonEncode({
+          "model": "text-davinci-003",
+          "prompt": '$message',
+          "temperature": 0,
+          "max_tokens": 100,
+          "top_p": 1,
+          "frequency_penalty": 0.0,
+          "presence_penalty": 0.0,
+          "stop": ["Human:", " AI:"]
+        }));
+
+    if (res.statusCode == 200) {
+      var data = jsonDecode(res.body.toString());
+      var msg = data['choices'][0]['text'];
+      return msg;
+    } else {
+      print("Failed to fetch data");
+    }
+  }
+}
---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const bgColor = Color.fromARGB(255, 170, 34, 255);
+const textColor = Color.fromARGB(220, 9, 217, 228);
+const chatBgColor = Color.fromARGB(255, 37, 3, 106);
+const kprimaryColor = Color.fromARGB(255, 227, 231, 233);
